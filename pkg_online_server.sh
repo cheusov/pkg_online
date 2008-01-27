d9c2f3a4cb1fd56a6f1f55effab514d5eed058be
Aleksey Cheusov <vle@gmx.net>
2008-01-27 16:43:04 +0000

    variables exported

--- pkg_online_server.sh
+++ pkg_online_server.sh
@@ -1,6 +1,6 @@
 if test -z "$PKG_ONLINE_SERVER"; then
-    PKG_ONLINE_SERVER=dictd.xdsl.by
+    export PKG_ONLINE_SERVER=dictd.xdsl.by
 fi
 if test -z "$PKG_ONLINE_PORT"; then
-    PKG_ONLINE_PORT=26280
+    export PKG_ONLINE_PORT=26280
 fi
